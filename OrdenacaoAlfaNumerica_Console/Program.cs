84d0012e63b8b379bd78b4536215a983328e6849
Elmo Junior <elmosouzajunior@gmail.com>
2021-08-24 21:05:42 -0300

    Exemplo de ordenação de listas no Console

---
+++ OrdenacaoAlfaNumerica_Console/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace OrdenacaoAlfaNumerica_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa("Gustavo Henrique", 36);
+            Pessoa pessoa2 = new Pessoa("Elmo Júnior", 39);
+            Pessoa pessoa3 = new Pessoa("Patyanne Carvalho", 35);
+            Pessoa pessoa4 = new Pessoa("Lucas Pedrosa", 36);
+
+            List<Pessoa> listaPessoas = new List<Pessoa>()
+            {
+                pessoa1,
+                pessoa2,
+                pessoa3,
+                pessoa4
+            };
+
+            List<Pessoa> listaPessoasOrdenada = new List<Pessoa>(listaPessoas);
+            
+            foreach (var pessoa in listaPessoas)
+                Console.WriteLine(pessoa);
+
+            listaPessoasOrdenada.Sort();    
+                        
+            Console.WriteLine("");
+            
+            foreach (var pessoa in listaPessoasOrdenada)
+                Console.WriteLine(pessoa);
+
+            Console.WriteLine("");
+
+            listaPessoasOrdenada.Sort((obj1, obj2) => obj1.Idade.CompareTo(obj2.Idade));
+
+            foreach (var pessoa in listaPessoasOrdenada)
+                Console.WriteLine(pessoa);
+
+            Console.WriteLine("");
+
+            int totalIdade = 0;
+            foreach (Pessoa pessoa in listaPessoas)
+            {
+                totalIdade += pessoa.Idade;
+            }
+
+            Console.WriteLine("A idade total das pessoas da lista é: " + totalIdade + " anos");
+
+            double idadeMedia = Convert.ToDouble(totalIdade) / listaPessoas.Count;
+            Console.WriteLine("A idade media das " + listaPessoas.Count + " pessoas é: " + idadeMedia.ToString("F2", CultureInfo.InvariantCulture) + " anos");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OrdenacaoAlfaNumerica_Console/Services/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OrdenacaoAlfaNumerica_Console
+{
+    public class Pessoa : IComparable
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+        
+        public override string ToString()
+        {
+            return $"[Nome: {Nome}, Idade: {Idade} anos]";
+        }
+
+        public int CompareTo(object obj)
+        {
+            Pessoa that = obj as Pessoa;
+            return this.Nome.CompareTo(that.Nome);
+        }       
+    }
+}
